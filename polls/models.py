ac58f5f768799580811a7db27d76ded2b3a21b07
Jesus Gonzalez <gonzalezjesus29s@gmail.com>
2019-04-18 20:44:10 -0500

    SecondC

--- polls/models.py
+++ polls/models.py
@@ -1,20 +1,12 @@
-from django.db import models
 import datetime
+from django.db import models
 from django.utils import timezone
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=300)
-    pub_date = models.DateTimeField('date published')
+class Parametro(models.Model):
+    voltaje = models.CharField(max_length=200)
+    corriente = models.CharField(max_length=200)
+    potencia = models.CharField(max_length=200)
+    fecha = models.DateTimeField('fecha')
     def __str__(self):
-        return self.question_text
-
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    def __str__(self):
-        return self.choice_text
+        return self.voltaje
+        return self.corriente
--- polls/views.py
+++ polls/views.py
@@ -1,21 +1,16 @@
-from django.http import HttpResponse
-from .models import Choice, Question
-from django.template import loader
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+from .models import Parametro
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        #return Question.objects.order_by('-pub_date')[:5]
+        print(Parametro.objects.all())
+        return Parametro.objects.all()[:1]
